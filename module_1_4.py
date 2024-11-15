dc520205c85c06297f855c46d7bb60763b63e1d2
First-TechWhiz <first.techwhiz@gmail.com>
2024-11-16 02:57:05 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Опишите какая сейчас погода за окном в двух словах: ')
+print(my_string)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
